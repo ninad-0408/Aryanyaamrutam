60b440153f7f07e413d913f74ccee5ae718e6663
Ninad Kalanke <ninadkalanke@gmail.com>
2020-07-04 20:15:14 +0530

    Home Page Completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
-$(document).ready(function(){
-    $("#navbartoggler").click(function(){
-   if( $("#navbartoggler").children('span').hasClass('fa-bars'))
-   {
-    $("#navbartoggler").children('span').removeClass('fa-bars');
-    $("#navbartoggler").children('span').addClass('fa-times');
-   }
-   else
-   {
-    $("#navbartoggler").children('span').removeClass('fa-times');
-    $("#navbartoggler").children('span').addClass('fa-bars');
-   }
-})
-})
+$(document).ready(function () {
+  $("#navbartoggler").click(function () {
+    if ($("#navbartoggler").children("span").hasClass("fa-bars")) {
+      $("#navbartoggler").children("span").removeClass("fa-bars");
+      $("#navbartoggler").children("span").addClass("fa-times");
+    } else {
+      $("#navbartoggler").children("span").removeClass("fa-times");
+      $("#navbartoggler").children("span").addClass("fa-bars");
+    }
+  });
+});
